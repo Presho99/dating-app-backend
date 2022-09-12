fb668c371fda1ff277204c4ab6206644fdea1803
Precious <Preshomondi@gmail.com>
2022-09-12 12:58:39 +0300

    Create a new endpoint in app controller

--- app/models/controllers/application_controller.rb
+++ app/models/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < Sinatra::Base
     { message: "Started my server using shotgun!" }.to_json
   end
 
+  get "/person/:person" do
+    person = Man.find_by(first_name:params[:person])|| Woman.find_by(first_name:params[:person])
+    person.to_json
+
+  end
+
 end
